0732d1f7a866b532c68906bd9cd24870d4a69075
lchege <laurachege9@gmail.com>
2024-07-19 13:12:14 +0300

    Initialized falsk app

--- message-word/app.py
+++ message-word/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, render_template
-
+import openai
+import os
 from twilio.rest import Client
 
 import random
+from dotenv import load_dotenv
 
+app = Flask(__name__)
+
+openai.api.key = os.getenv("OPENAI_API_KEY")
+twilio_client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
+twilio_phone_number = os.getenv("TWILIO_PHONE_NUMBER")
